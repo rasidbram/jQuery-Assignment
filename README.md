e99bc5b06242c4516c951c17e0a9bda76b71fe5a
Your Name <rashidaynur.ibram@yandex.com>
2019-08-07 15:32:15 +0100

    It could be better

---
+++ README.md
@@ -0,0 +1,35 @@
+## jQuery-Assignment
+
+**_Deadline Wednesday evening_**
+
+
+The tasks are all about Javascript and jQuery exercise. It is going to be very beneficial for you. Try to gain the most from it..
+
+### Description
+
+1. First of all fork this repository. After forking you should see this repo in your own GitHub account. 
+
+2. Now you have the repo in your GitHub. Clone the repo. Now you can work locally.
+
+3. The project contains `index.html`,  `style.css` and `app.js`. The css and js files are already connected to the html file. So you may start writing js code right away.  But don't forget to include jQuery in the `index.html`. If you preview the html, you will see no functioning web-page. Your task is to make them functioning.
+
+#### Here are the tasks to do, you must use jQuery to solve them
+
+- `Delete` buttons should remove the topics (`li` elements). 
+- `Add` button should add a topic (an `li` element inside 2 `span` elements).
+- There is a `checkbox` under the topics. It should hide all topics when checked, unhide when unchecked. (hide all `ul` elements)
+- There is search field above. It should filter the topics (filter `li` elements). It should be case-insensitive.
+- After finishing the tasks; do `add` `commit` and `push` to update the repository on your GitHub account, and share the link of the project like [this](https://livecodingonline.github.io/jQuery-Assignment/) via Trello. *Remember: Repository settings > GitHub Pages > Source (master)*
+
+###### Bonus
+- You may add your own styles and possibly add more items and functionalities to the project. You may use this project for your portfolio. Even it could be shared via LinkedIn rightaway.
+
+
+---
+
+If you have a problem in forking cloning and starting the issue locally; please ask for an help immediately. These are not the tasks for this week, use your energies wisely. For the other bullet points; push yourself and try to solve them on your own but again you shouldn't be working in one bullet point more than 3 hours, please ask for an help when you stuck.
+
+Note: You may get help from [here](https://www.youtube.com/watch?v=g7FYeqnJO0k&list=PLHhUqTSc-34maI6aRjVoqnLCZpfgnl7EH&index=2&t=0s)
+
+
+Good Luck! 
---
+++ mainapp.js
@@ -0,0 +1,190 @@
+// $(function () {
+//     for (var i = 0; i < 4; i++) {
+//         $(".delete").eq(i).click(function () {
+//             $("li").eq(i).hide();
+//         });
+
+//     }
+// })
+
+// use for loop!!!! 
+$(".delete").eq(0).click(function () {
+  $("li").eq(0).hide();
+  $("label").text("Hide all topic").css("color","red")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+        
+})
+$("#hide").eq(0).click(function () {
+  $("li").show();
+ });
+
+
+$(".delete").eq(1).click(function () {
+  $("li").eq(1).hide();
+  $("label").text("Hide all topic").css("color","red")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+});
+$("#hide").eq(1).click(function () {
+  $("li").show();
+});
+
+$(".delete").eq(2).click(function () {
+  $("li").eq(2).hide();
+  $("label").text("Hide all topic").css("color","red")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+});
+$("#hide").eq(2).click(function () {
+  $("li").show();
+});
+
+
+$(".delete").eq(3).click(function () {
+  $("li").eq(3).hide();
+  $("label").text("Hide all topic").css("color","red")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+});
+$("#hide").eq(3).click(function () {
+  $("li").show();
+});
+
+
+
+// to add two span inside list
+$("form").submit(function (e) {
+  if ($(".input").val() !== "") {
+    var inputText = $(".input").val();
+    var spanText = "delete";
+    var spans =
+      "<li>" +
+      ("<span class='name'>" + inputText + "</span>") +
+      ("<span class='delete'>" + spanText + "</span>") +
+      "</li>";
+
+    $("ul").append(spans);
+    $(".delete").eq(4).click(function () {
+      $("li").eq(4).hide();
+      $("label").text("Hide all topics").css("color","red")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+      
+    });
+    $("#hide").eq(4).click(function () {
+      $("li").show();
+    });
+    $(".delete").eq(5).click(function () {
+      $("li").eq(5).hide();
+      $("label").text("Hide all topics").css("color","red")
+      .fadeOut(200)
+      .fadeIn(200)
+      .fadeOut(200)
+      .fadeIn(200)
+      .fadeOut(200)
+      .fadeIn(200);
+      
+    });
+    $("#hide").eq(5).click(function () {
+      $("li").show();
+    });
+
+    $(".input").val("");
+  }
+  return false;
+});
+
+// to hide all topics and show again
+
+$(function () {
+  $("#hide").click(function () {
+    if ($(this).is(":checked")) {
+      var muzik2= new Audio( "audio/Cave.mp3");
+        muzik2.play();
+      $("ul").hide();
+      $("label")
+        .text("Show all topics")
+        .css("color", "red");
+      $("label")
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200)
+        .fadeOut(200)
+        .fadeIn(200);
+    } else {
+      var muzik1= new Audio( "audio/News.mp3");
+        muzik1.play();
+      $("label")
+        .text("Hide all topics")
+        .css("color", "#444");
+      $("ul").show();
+    }
+  });
+});
+
+//  search words,letters
+
+$(".input1").on("keyup", function () {
+  var value = $(this).val().toLowerCase();
+  $("ul li").filter(function () {
+  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    
+  });
+});
+
+// HIGHLIGHT FUNCTION. DO IT AGAIN
+
+// $("input").keyup(function(){
+//   searchHighlight($(this).val())
+// })
+// var searchText=$('.input').val();
+// function searchHighlight(searchText){
+//   if (searchText){
+//     var searchExp=new RegExp(searchText,'ig')
+//     var content=$("ul li").text();
+//     var matches=content.match(searchExp);
+//     if(matches){
+//       $("ul li").html(content.replace(searchExp, function(match){
+//         return "<span class='highlight'>"+match+"</span>"
+//       }))
+//     }
+//   }
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
